1e744c1f118fcfdbd2508a09c509bb37ba36b1c9
DvoidX-io <devanshchavda39@gmail.com>
2026-07-21 00:26:30 +0530

    Added new Codeforces solutions

---
+++ 800/stairpeakornone_1950A.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cin >> n;
+    for(int i=0;i<n;i++){
+    
+    int a,b,c;
+    cin  >> a >> b >> c;
+    if(a<b && b<c){
+        cout << "Stair" << endl;}
+        
+        else if (a<b && b>c){
+            cout << "Peak" << endl;
+        }
+        else {
+            cout << "None" << endl;
+        }
+
+    
+}
+}
