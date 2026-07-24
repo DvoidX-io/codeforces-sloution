1e5c5f5face02fcf375760690f3649ae289f4703
DvoidX-io <devanshchavda39@gmail.com>
2026-07-24 23:19:20 +0530

    800 level solutons

---
+++ 800/shortsubstring1367A.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int t;
+    cin >> t;
+
+    while(t--){
+
+        string b;
+        cin >> b;
+
+        for(int i = 0; i < b.size()-1; i+=2)
+        cout << b[i];
+
+        cout << b.back() << endl;
+    }
+    return 0;
+}
