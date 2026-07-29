674f69ee38355cf85edd3d89e51396b35752790c
DvoidX-io <devanshchavda39@gmail.com>
2026-07-29 22:55:12 +0530

    800 level solution

--- 800/sale34B.cpp
+++ 800/sale34B.cpp
@@ -1,4 +1,5 @@
-<iostream>
+#include <iostream>
+#include <algorithm>
 using namespace std;
 
 int main(){
@@ -10,7 +11,7 @@ int main(){
     for(int i = 0; i < n; i++)
     cin >> a[i];
 
-    sort(a, a+n);
+    sort(a, a + n);
 
     int ans = 0;
 
