02ab379d8a28e1fa651f86d975c42c78e52738cd
DvoidX-io <devanshchavda39@gmail.com>
2026-08-03 23:30:54 +0530

    800 level solutions

---
+++ 800/requiredreminder1374A.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        long long x, y, n;
+        cin >> x >> y >> n;
+
+        long long k = n - (n % x);
+        k += y;
+
+        if (k > n)
+            k -= x;
+
+        cout << k << endl;
+    }
+
+    return 0;
+}
